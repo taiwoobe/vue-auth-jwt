ce7d46f5095e5ed6ca12fdf0ff2928f199126d08
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-10-21 11:54:41 +0100

    Fixing the Login & Register component

---
+++ src/components/authentication/Login.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <form class="login" @submit.prevent="login">
+           <h1>Sign in</h1>
+           <label>Email</label>
+           <input required v-model="email" type="email" placeholder="Email Address"/>
+           <label>Password</label>
+           <input required v-model="password" type="password" placeholder="Password"/>
+           <hr/>
+           <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            login() {
+                const { email, password } = this;
+                this.$store.dispatch("login", {email, password}).then(() => {
+                    this.$router.push('/');
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
